6791da6ec9d24cee1fbc50f2170bce239d3f77d5
Sean Yu <yusean275@gmail.com>
2026-03-31 15:21:37 +0800

    Admin dashboard

---
+++ SaleSync/Controllers/AdminController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SaleSync.Models;
+using System.Collections.Generic;
+
+namespace SaleSync.Controllers
+{
+    public class AdminController : Controller
+    {
+        private static List<InventoryItems> inventoryItems = new List<InventoryItems>();
+
+        public IActionResult Dashboard()
+        {
+            return View("AdminDashboard");
+        }
+
+        [HttpGet]
+        public IActionResult Inventory()
+        {
+            return View(inventoryItems);
+        }
+
+        [HttpPost]
+        public IActionResult AddInventory(InventoryItems item)
+        {
+            if (item != null)
+            {
+                inventoryItems.Add(item);
+            }
+
+            return RedirectToAction("Inventory");
+        }
+
+        public IActionResult ManageAccounts()
+        {
+            return View();
+        }
+    }
+}
---
+++ SaleSync/Models/IventoryItems.cs
@@ -0,0 +1,14 @@
+﻿namespace SaleSync.Models
+{
+    public class InventoryItems
+    {
+        public string ItemCategory { get; set; }
+        public string ItemName { get; set; }
+        public string ItemID { get; set; }
+        public decimal PurchasePrice { get; set; }
+        public int Quantity { get; set; }
+        public string StockSupplier { get; set; }
+        public string DateAcquired { get; set; }
+        public string ExpirationDate { get; set; }
+    }
+}
