2d36e74b65695bc859e62d28403b9e5cb57d878f
Sean Yu <yusean275@gmail.com>
2026-03-31 20:14:52 +0800

    Cashier and cart

---
+++ SaleSync/Controllers/CashierController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SaleSync.Controllers
+{
+    public class CashierController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View("CashierDashboard");
+        }
+    }
+}
