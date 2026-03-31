593f0d7a9fd8d38b1ca3d5ecafa42a89d696cb6c
Sean Yu <yusean275@gmail.com>
2026-03-31 12:51:58 +0800

    Log In Page

--- SaleSync/Controllers/HomeController.cs
+++ SaleSync/Controllers/HomeController.cs
@@ -21,5 +21,10 @@ namespace SaleSync.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult AdminDashboard()
+        {
+            return View();
+        }
     }
 }
