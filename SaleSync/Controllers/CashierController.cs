72d79b71f2ef85c7765b2a83dee08a017d31ba22
Sean Yu <yusean275@gmail.com>
2026-04-20 21:14:22 +0800

    home page UI

--- SaleSync/Controllers/CashierController.cs
+++ SaleSync/Controllers/CashierController.cs
@@ -9,7 +9,7 @@ namespace SaleSync.Controllers
 {
     public class CashierController : Controller
     {
-        private readonly string connectionString = "Server=IANPC;Database=SaleSync;Trusted_Connection=True;Encrypt=False;";
+        private readonly string connectionString = "Server=(localdb)\\MSSQLLocalDB;Database=SaleSync;Trusted_Connection=True;TrustServerCertificate=True;";
 
         // DASHBOARD
         public IActionResult Dashboard()
